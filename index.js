454555a89b33948b3dd076265884f9ca2a19ce95
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2023-08-07 11:20:44 +0530

    env port variable

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
 
 app.get("/", (req, res) => {
