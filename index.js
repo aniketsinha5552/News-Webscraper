5e4ff10f3d1b3f66a52980adca89d1c8eb99436f
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2023-08-07 11:46:29 +0530

    Home page html

--- index.js
+++ index.js
@@ -14,7 +14,32 @@ app.listen(process.env.PORT || 5000, () => {
 });
 
 app.get("/", (req, res) => {
-   res.send("<h1>News API</h1>");
+   let home = `
+   <div style='color: rgb(1, 46, 46);display: grid;place-items: center;'>
+   <a href="https://github.com/aniketsinha5552/News-Webscrapper">
+       <h2>News Webscrapper API Documentation</h2>
+   </a>
+   <h4><a href="https://github.com/aniketsinha5552">Aniket Sinha</a></h4>
+   <h3>API Endpoints</h3>
+   <ul>
+       <li>
+           <h4>GET /news/all</h4>
+           <p>Get all the news from all the sources</p>
+       </li>
+       <li>
+           <h4>GET /news/:source</h4>
+           <p>Get all the news from a particular source (toi/ thehindu/ cnn/ aljazeera)</p>
+       </li>
+       <li>
+           <h4>POST /news/search/:query</h4>
+           <p>Search news</p>
+           <p>Body: {search: "query"}</p>
+       </li>
+   </ul>
+   
+  </div>
+   `
+   res.send(home);
    // fs.readFile("./home.html", null,(err, data) => {
    //    if(err) {
    //       console.log(err);
